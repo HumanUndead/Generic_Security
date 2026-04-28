35a3d8de91bd63f5b12bda5c13f98c3df2e99f17
yosef <yosef@DESKTOP-QJM53GJ>
2026-04-28 15:49:08 +0300

    up

--- KenSoftware.Security/obj/Debug/net10.0/KenSoftware.Security.AssemblyInfo.cs
+++ KenSoftware.Security/obj/Debug/net10.0/KenSoftware.Security.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KenSoftware.Security")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be32c00d48a0524ac9d2af879410238e08f26af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+830c79bd6e8dad1f0e636c8942172d625514a9aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("KenSoftware.Security")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KenSoftware.Security")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
